abc3b186c3c81a23ebe95f4d2662e6c77ee4ee5f
cheesysamira <samiram18@nycstudents.net>
2023-10-01 14:35:20 -0400

    Add comments for readability

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -6,6 +6,7 @@ public class TipCalculator {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the tip calculator!");
 
+        //initialize variables and inputs
         System.out.print("How many people are in your group: ");
         int peopleInGroup = scan.nextInt();
 
@@ -17,7 +18,10 @@ public class TipCalculator {
         int numOfItems = 0;
         final double END = -1.00;
 
+        // Create the array list to store items
         ArrayList<String> items = new ArrayList<>();
+
+        //while loop until user inputs -1
         while (itemCost != END){
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             itemCost = scan.nextDouble();
@@ -30,12 +34,14 @@ public class TipCalculator {
             }
         }
 
+        //calculations with new variables
         double totalTip = Math.round(cost * (tipPercentage / 100.00) * 100) /100.00;
         double totalBillWithTip = Math.round((totalTip + cost)*100.00)/100.00;
         double perPeronBeforeTip = Math.round((cost/peopleInGroup)*100.00)/100.00;
         double tipPerPerson = Math.round((totalTip/peopleInGroup)*100.00)/100.00;
         double totalCostPerPerson = Math.round((tipPerPerson + perPeronBeforeTip)*100.00)/100.00;
 
+        //print statements for calculator info
         System.out.println("------------------------------");
         System.out.println("Total bill before tip: $" + cost);
         System.out.println("Total percentage: " + tipPercentage + "%");
@@ -47,18 +53,23 @@ public class TipCalculator {
         System.out.println("------------------------------");
         System.out.println("Items ordered:");
 
+        //for loop that prints out every item ordered
         for (String item : items) {
             System.out.println(item);
         }
 
+        //freestyle for code
         System.out.println("------------------------------");
         System.out.print("How much money did you give?: $");
+        //inputs the money they gave to cashier
         double moneyGiven = scan.nextDouble();
         moneyGiven = Math.round(moneyGiven*100.00)/100.00;
 
+        //calculates the change from bill with tip
         double change = Math.round((moneyGiven-totalBillWithTip)*100.00)/100.00;
         System.out.println("Change: $" + change);
 
+        //split change calculation
         double changePerPerson = Math.round((change/peopleInGroup)*100.00)/100.00;
         System.out.println("If you want to split the change, each person will receive: $" + changePerPerson);
     }
